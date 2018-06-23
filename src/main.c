b214c221720487dc044c5c0e63715189fc75b26a
monstruosoft <monstruosoft@users.noreply.github.com>
2018-06-23 12:52:31 -0500

    Nuevas opciones
    Se agregaron opciones para insertar patrones predeterminados como
    'gliders' y 'glider guns' sacados de Wikipedia. Hay en total 9
    patrones además del modo normal.
    
    El código, que comenzó, como un programa sencillo se convirtió en
    un monstruo lleno de código repetido que se podría simplificar con
    un poco de trabajo pero de momento creo que con esto doy por
    terminado el reto de NK.

--- src/main.c
+++ src/main.c
@@ -61,9 +61,49 @@ bool creation = false;
 bool running = false;
 bool shuffle = false;
 bool help = true;
+int pattern = 0;
 int speed = 1;
 int size = 1;
 int total_frames = 0;
+
+enum {NONE, GLIDER, LWSS, RPENTOMINO, DIEHARD, ACORN, GOSPERGLIDERGUN, INFINITE1, INFINITE2, INFINITE3};
+char glider[3][3] = {{0,0,1},
+                     {1,0,1},
+                     {0,1,1}};
+char lwss[4][5] = {{0,1,1,0,0},
+                   {1,1,0,1,1},
+                   {0,1,1,1,1},
+                   {0,0,1,1,0}};
+char rpentomino[3][3] = {{0,1,1},
+                         {1,1,0},
+                         {0,1,0}};
+char diehard[3][8] = {{0,0,0,0,0,0,1,0},
+                      {1,1,0,0,0,0,0,0},
+                      {0,1,0,0,0,1,1,1}};
+char acorn[3][7] = {{0,1,0,0,0,0,0},
+                    {0,0,0,1,0,0,0},
+                    {1,1,0,0,1,1,1}};
+char gosperglidergun[9][36] = {{0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+                               {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0},
+                               {0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+                               {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,1,1},
+                               {1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                               {1,1,0,0,0,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0},
+                               {0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,1,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0},
+                               {0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                               {0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+char infinite1[6][8] = {{0,0,0,0,0,0,1,0},
+                        {0,0,0,0,1,0,1,1},
+                        {0,0,0,0,1,0,1,0},
+                        {0,0,0,0,1,0,0,0},
+                        {0,0,1,0,0,0,0,0},
+                        {1,0,1,0,0,0,0,0}};
+char infinite2[5][5] = {{1,1,1,0,1},
+                        {1,0,0,0,0},
+                        {0,0,0,1,1},
+                        {0,1,1,0,1},
+                        {1,0,1,0,1}};
+char infinite3[1][39] = {1,1,1,1,1,1,1,1,0,1,1,1,1,1,0,0,0,1,1,1,0,0,0,0,0,0,1,1,1,1,1,1,1,0,1,1,1,1,1};
 
 
 
@@ -71,9 +111,9 @@ void add_cell(int mouse_x, int mouse_y) {
     bool x_valid = (mouse_x > -1 && mouse_x < SCR_WIDTH) ? true : false;
     bool y_valid = (mouse_y > -1 && mouse_y < SCR_HEIGHT) ? true : false;
     if (creation && x_valid && y_valid) {
-        if (size == 1)
+        if (size == 1 && pattern == NONE)
             universe[mouse_x][mouse_y] = 1;
-        else {
+        else if (size > 1 && pattern == NONE) {
             int offset = size / 2;
             int x1 = mouse_x - offset;
             int x2 = mouse_x + offset;
@@ -81,13 +121,103 @@ void add_cell(int mouse_x, int mouse_y) {
             int y2 = mouse_y + offset;
 
             x1 = x1 > -1 ? x1 : 0;
-            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH - 1;
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
             y1 = y1 > -1 ? y1 : 0;
-            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT - 1;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
             for (int x = x1; x < x2; x++)
                 for (int y = y1; y < y2; y++)
                     if (!shuffle) universe[x][y] = 1;
                     else universe[x][y] = rand() % 2;
+        }
+        else if (pattern == GLIDER) {
+            int x2 = mouse_x + 3;   // Glider X size
+            int y2 = mouse_y + 3;   // Glider Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = glider[j][i];
+        }
+        else if (pattern == LWSS) {
+            int x2 = mouse_x + 5;   // LWSS X size
+            int y2 = mouse_y + 4;   // LWSS Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = lwss[j][i];
+        }
+        else if (pattern == RPENTOMINO) {
+            int x2 = mouse_x + 3;   // R-Pentomino X size
+            int y2 = mouse_y + 3;   // R-Pentomino Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = rpentomino[j][i];
+        }
+        else if (pattern == DIEHARD) {
+            int x2 = mouse_x + 8;   // Diehard X size
+            int y2 = mouse_y + 3;   // Diehard Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = diehard[j][i];
+        }
+        else if (pattern == ACORN) {
+            int x2 = mouse_x + 7;   // Acorn X size
+            int y2 = mouse_y + 3;   // Acorn Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = acorn[j][i];
+        }
+        else if (pattern == GOSPERGLIDERGUN) {
+            int x2 = mouse_x + 36;   // Gosper Glider Gun X size
+            int y2 = mouse_y + 9;    // Gosper Glider Gun Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = gosperglidergun[j][i];
+        }
+        else if (pattern == INFINITE1) {
+            int x2 = mouse_x + 8;   // Infinite 1 X size
+            int y2 = mouse_y + 6;   // Infinite 1 Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = infinite1[j][i];
+        }
+        else if (pattern == INFINITE2) {
+            int x2 = mouse_x + 5;   // Infinite 2 X size
+            int y2 = mouse_y + 5;   // Infinite 2 Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = infinite2[j][i];
+        }
+        else if (pattern == INFINITE3) {
+            int x2 = mouse_x + 39;   // Infinite 3 X size
+            int y2 = mouse_y + 1;    // Infinite 3 Y size
+
+            x2 = x2 < SCR_WIDTH ? x2 : SCR_WIDTH;
+            y2 = y2 < SCR_HEIGHT ? y2 : SCR_HEIGHT;
+            for (int x = mouse_x, i = 0; x < x2; x++, i++)
+                for (int y = mouse_y, j= 0; y < y2; y++, j++)
+                    universe[x][y] = infinite3[j][i];
         }
     }
 }
@@ -162,6 +292,36 @@ void logic(ALLEGRO_EVENT *event) {
                 speed = speed < 4 ? speed + 1 : 1;
                 al_set_timer_speed(timer, ALLEGRO_BPS_TO_SECS(speed * 1.5 * 10));
                 break;
+            case ALLEGRO_KEY_1:
+                pattern = GLIDER;
+                break;
+            case ALLEGRO_KEY_2:
+                pattern = LWSS;
+                break;
+            case ALLEGRO_KEY_3:
+                pattern = RPENTOMINO;
+                break;
+            case ALLEGRO_KEY_4:
+                pattern = DIEHARD;
+                break;
+            case ALLEGRO_KEY_5:
+                pattern = ACORN;
+                break;
+            case ALLEGRO_KEY_6:
+                pattern = GOSPERGLIDERGUN;
+                break;
+            case ALLEGRO_KEY_7:
+                pattern = INFINITE1;
+                break;
+            case ALLEGRO_KEY_8:
+                pattern = INFINITE2;
+                break;
+            case ALLEGRO_KEY_9:
+                pattern = INFINITE3;
+                break;
+            case ALLEGRO_KEY_0:
+                pattern = NONE;
+                break;
             }
         }
     }
@@ -190,12 +350,22 @@ void update() {
                 al_draw_pixel(x, y, al_map_rgb(255, 255, 255));
 
     if (help) {
-        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  0, 0, "• Usa el mouse para colocar células.");
-        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 10, 0, "• Presiona Tab para mostrar/ocultar esta ayuda.");
-        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 20, 0, "• Presiona Espacio para iniciar/detener la simulación.");
-        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 30, 0, "• Presiona +/- para aumentar/reducir el tamaño del puntero del mouse: %d.", size);
-        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 40, 0, "• Presiona R para activar/desactivar el modo de células aleatorias: %s.", shuffle ? "ON" : "OFF");
-        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 50, 0, "• Presiona F para cambiar la velocidad de la simulación: x%d.", speed);
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,   0, 0, "• Usa el mouse para colocar células.");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  10, 0, "• Presiona Tab para mostrar/ocultar esta ayuda.");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  20, 0, "• Presiona Espacio para iniciar/detener la simulación.");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  30, 0, "• Presiona +/- para aumentar/reducir el tamaño del puntero del mouse: %d.", size);
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  40, 0, "• Presiona R para activar/desactivar el modo de células aleatorias: %s.", shuffle ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  50, 0, "• Presiona F para cambiar la velocidad de la simulación: x%d.", speed);
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  60, 0, "• Presiona 1 para insertar GLIDERS: %s.", pattern == 1 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  70, 0, "• Presiona 2 para insertar LWSS-SPACESHIPS: %s.", pattern == 2 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  80, 0, "• Presiona 3 para insertar R-PENTOMINOS: %s.", pattern == 3 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0,  90, 0, "• Presiona 4 para insertar DIEHARDS: %s.", pattern == 4 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 100, 0, "• Presiona 5 para insertar ACORNS: %s.", pattern == 5 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 110, 0, "• Presiona 6 para insertar GOSPER GLIDER GUNS: %s.", pattern == 6 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 120, 0, "• Presiona 7 para insertar INFINITE-1: %s.", pattern == 7 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 130, 0, "• Presiona 8 para insertar INFINITE-2: %s.", pattern == 8 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 140, 0, "• Presiona 9 para insertar INFINITE-3: %s.", pattern == 9 ? "ON" : "OFF");
+        al_draw_textf(font, al_map_rgb(255, 255, 255), 0, 150, 0, "• Presiona 0 para volver al modo normal.");
     }
 }
 
